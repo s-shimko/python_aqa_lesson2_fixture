06d47c03e4c2135ea1baf788d4f942cf12088099
s.shimko <sergey1shimko@gmail.com>
2018-02-18 13:24:59 +0300

    [+] added list comparing to Groups

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 def test_delete_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups[0:1] = []
+    assert old_groups == new_groups
+
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,11 +1,22 @@
 from model.group import Group
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
+    group = Group(name="modified_group")
+    group.id = old_groups[0].id
     if app.group.count() == 0:
         app.group.create(Group(name="test_group"))
-    app.group.modify_first_group(Group(name="modified_group"))
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(header="test_header"))
-    app.group.modify_first_group(Group(header="modified_header"))
+
+# def test_modify_group_header(app):
+#     old_groups = app.group.get_group_list()
+#     if app.group.count() == 0:
+#         app.group.create(Group(header="test_header"))
+#     app.group.modify_first_group(Group(header="modified_header"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
