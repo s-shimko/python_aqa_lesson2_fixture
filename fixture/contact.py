0bc628df70ee5a880dfefaacf6825afe5144e384
s.shimko <sergey1shimko@gmail.com>
2018-02-25 16:54:20 +0300

    [+] add parametrized in contacts

--- fixture/contact.py
+++ fixture/contact.py
@@ -28,16 +28,16 @@ class ContactHelper:
         wd.find_element_by_name("title").send_keys(contact.title)
         wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").send_keys(contact.work)
+        wd.find_element_by_name("home").send_keys(contact.homephone)
+        wd.find_element_by_name("mobile").send_keys(contact.mobilephone)
+        wd.find_element_by_name("work").send_keys(contact.workphone)
         wd.find_element_by_name("fax").send_keys(contact.fax)
         wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").send_keys(contact.email)
-        wd.find_element_by_name("email3").send_keys(contact.email)
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").send_keys(contact.email3)
         wd.find_element_by_name("homepage").send_keys(contact.homepage)
         wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
+        wd.find_element_by_name("phone2").send_keys(contact.secondaryphone)
         wd.find_element_by_name("notes").send_keys(contact.notes)
 
     def return_contact_page(self):
@@ -46,7 +46,7 @@ class ContactHelper:
 
     def open_add_contact_page(self):
         wd = self.app.wd
-        if not (wd.current_url.endswith("/edit.php") and wd.find_elements_by_name("submit") > 0):
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("submit")) > 0):
             wd.find_element_by_link_text("add new").click()
 
     def open_contact_list_page(self):
--- test/contact/test_add_contact.py
+++ test/contact/test_add_contact.py
@@ -1,16 +1,37 @@
 # -*- coding: utf-8 -*-
-
 from model.contact import Contact
+import pytest
+import random
+import string
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    # symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "_" + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [
+           #     Contact(firstname="", middlename="",
+           #            lastname="", nickname="",
+           #            title="", company="", address="",
+           #            homephone="", mobilephone="", workphone="",
+           #            fax="", email="", email2="", email3="",
+           #            homepage="",
+           #            address2="", secondaryphone="", notes="")] + \
+           # [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
+            lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
+            title=random_string("title", 10), company=random_string("company", 10), address=random_string("address", 10),
+            homephone=random_string("homephone", 10), mobilephone=random_string("mobilephone", 10), workphone=random_string("workphone", 10),
+            fax=random_string("fax", 10),
+            email=random_string("email", 10), email2=random_string("email2", 10), email3=random_string("email3", 10),
+            homepage=random_string("homepage", 10), address2=random_string("address2", 10),
+            secondaryphone=random_string("secondaryphone", 10), notes=random_string("notes", 10))
+    for i in range(2)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="firstname", middlename="middlename",
-                      lastname="lastname", nickname="nickname",
-                      title="title", company="company", address="address",
-                      homephone="home", mobilephone="mobile", workphone="work",
-                      fax="fax", email="email", email2="email2", email3="email3",
-                      homepage="homepage",
-                      address2="address2", secondaryphone="phone2", notes="notes")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
