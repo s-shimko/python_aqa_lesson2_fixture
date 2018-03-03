2e491725c93c7176df66597f143a0b89052b735b
s.shimko <sergey1shimko@gmail.com>
2018-03-03 23:13:19 +0300

    [+] added json generator and load it in method

--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -13,8 +13,8 @@ except getopt.GetoptError as err:
     getopt.usage()
     sys.exit(2)
 
-n = 5
-f = "/data/groups.json"
+n = 2
+f = "data/groups.json"
 
 for o, a in opts:
     if o == "-n":
@@ -33,7 +33,8 @@ testdata = [Group(name="", header="", footer="")] + [
     for i in range(n)
 ]
 
-file = config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".." + f)
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
