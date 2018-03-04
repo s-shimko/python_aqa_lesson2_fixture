60564ae84a4317d8105c50b896c16a023a9d2e2d
s.shimko <sergey1shimko@gmail.com>
2018-03-04 21:35:50 +0300

    [+] added contact json data usage

---
+++ data/contacts.py
@@ -0,0 +1,25 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="firstname1", middlename="middlename1",
+            lastname="lastname1", nickname="nickname1",
+            title="title1", company="company1",
+            address="address1",
+            homephone="homephone1", mobilephone="mobilephone1",
+            workphone="workphone1",
+            fax="fax1",
+            email="email1", email2="email1", email3="email1",
+            homepage="homepage1", address2="address21",
+            secondaryphone="secondaryphone1", notes="notes1"),
+    Contact(firstname="firstname2", middlename="middlename2",
+            lastname="lastname2", nickname="nickname2",
+            title="title2", company="company2",
+            address="address2",
+            homephone="homephone2", mobilephone="mobilephone2",
+            workphone="workphone2",
+            fax="fax2",
+            email="email2", email2="email2", email3="email2",
+            homepage="homepage2", address2="address2",
+            secondaryphone="secondaryphone2", notes="notes2"),
+]
--- test/contact/test_add_contact.py
+++ test/contact/test_add_contact.py
@@ -1,36 +1,8 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    # symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "_" + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [
-           #     Contact(firstname="", middlename="",
-           #            lastname="", nickname="",
-           #            title="", company="", address="",
-           #            homephone="", mobilephone="", workphone="",
-           #            fax="", email="", email2="", email3="",
-           #            homepage="",
-           #            address2="", secondaryphone="", notes="")] + \
-           # [
-    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
-            lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
-            title=random_string("title", 10), company=random_string("company", 10), address=random_string("address", 10),
-            homephone=random_string("homephone", 10), mobilephone=random_string("mobilephone", 10), workphone=random_string("workphone", 10),
-            fax=random_string("fax", 10),
-            email=random_string("email", 10), email2=random_string("email2", 10), email3=random_string("email3", 10),
-            homepage=random_string("homepage", 10), address2=random_string("address2", 10),
-            secondaryphone=random_string("secondaryphone", 10), notes=random_string("notes", 10))
-    for i in range(1)
-]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
