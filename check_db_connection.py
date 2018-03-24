8403c071e00ac118e090a4fb1eb979651336c456
s.shimko <sergey1shimko@gmail.com>
2018-03-24 11:26:08 +0300

    [+] orm lecture data added

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,30 @@
-import mysql.connector
+from fixture.orm import ORMFixture
+from model.group import Group
 
-connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+#
+# try:
+#     groups = db.get_group_list()
+#     for group in groups:
+#         print(group)
+#     print(len(groups))
+# finally:
+#     pass
+
+# print("=======================================")
+#
+# try:
+#     l = db.get_contact_list()
+#     for item in l:
+#         print(item)
+#     print(len(l))
+# finally:
+#     pass # db.destroy()
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    l = db.get_contacts_in_group(Group(id='90'))
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    connection.close()
+    pass  # db.destroy()
